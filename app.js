63020fccb38fd8a38469b06b87b2c6e0543b6a28
i5haan <ishaankohli4321@gmail.com>
2018-05-29 07:28:27 +0530

    Serial Start

--- app.js
+++ app.js
@@ -1,42 +1,10 @@
-var request=require("request");
+var express=require("express");
+var app=express();
+var serial=require("./serial");
+serial.run();
 
-var alpha=[0,0,0];
-var prevOmega=[0,0,0];
-var currentOmega=[0,0,0];
 
-var checkGyro=function(){
-	request.get("http://192.168.43.1:8080/sensors.json?sense=gyro",function(e,r,b)
-		{
-			if(!e && r.statusCode==200)
-			{
-				var d=JSON.parse(b);
-				if(d.gyro)
-				{
-					currentOmega=d.gyro.data[d.gyro.data.length-1][1];
-					for(var i=0;i<3;i++)
-					{
-						// alpha[i]=alpha[i]+((d.gyro.data[d.gyro.data.length-1][1][i]-d.gyro.data[d.gyro.data.length-2][1][i])*(d.gyro.data[d.gyro.data.length-1][0]-d.gyro.data[d.gyro.data.length-2][0])*180)/3.14;
-						alpha[i]=alpha[i]+((currentOmega[i]-prevOmega[i])*0.025*180)/3.14;
-						if(alpha[i]>180)
-						{
-							alpha[i]=alpha[i]-360;
-						}
-						else if(alpha[i]<-180)
-						{
-							alpha[i]=alpha[i]+360;
-						}
-					}
-				}
-				
-			}
-			else
-			{
-				console.log(e);
-				alpha=[0,0,0];
-				prevOmega=[0,0,0];
-			}
-		});
-	console.log(alpha);
-}
-
-var gyroIntervalId=setInterval(checkGyro,10);
+app.listen(8080,function()
+	{
+		console.log("Server is also start");
+	});
